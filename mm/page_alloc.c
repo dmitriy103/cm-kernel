71c2867507ee22805f51a0d96c63870757bdd5f8
Mel Gorman <mel@csn.ul.ie>
2010-11-22 15:43:51 +0000

    mm: vmscan: Reclaim order-0 and use compaction instead of lumpy reclaim
    Lumpy reclaim is disruptive. It reclaims a large number of pages and ignores
    the age of the pages it reclaims. This can incur significant stalls and
    potentially increase the number of major faults.
    
    Compaction has reached the point where it is considered reasonably stable
    (meaning it has passed a lot of testing) and is a potential candidate for
    displacing lumpy reclaim. This patch introduces an alternative to lumpy
    reclaim whe compaction is available called reclaim/compaction. The basic
    operation is very simple - instead of selecting a contiguous range of pages
    to reclaim, a number of order-0 pages are reclaimed and then compaction is
    later by either kswapd (compact_zone_order()) or direct compaction
    (__alloc_pages_direct_compact()).
    
    Signed-off-by: Mel Gorman <mel@csn.ul.ie>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -2102,6 +2102,19 @@ rebalance:
 		/* Wait for some write requests to complete then retry */
 		wait_iff_congested(preferred_zone, BLK_RW_ASYNC, HZ/50);
 		goto rebalance;
+	} else {
+		/*
+		 * High-order allocations do not necessarily loop after
+		 * direct reclaim and reclaim/compaction depends on compaction
+		 * being called after reclaim so call directly if necessary
+		 */
+		page = __alloc_pages_direct_compact(gfp_mask, order,
+					zonelist, high_zoneidx,
+					nodemask,
+					alloc_flags, preferred_zone,
+					migratetype, &did_some_progress);
+		if (page)
+			goto got_pg;
 	}
 
 nopage:
